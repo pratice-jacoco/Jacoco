cf7f66d9ab6deb4ccf51abc1bd349e464193a074
choejonghyeon <choijonghyen@gmail.com>
2024-12-07 11:43:53 +0900

    feat: div

--- src/main/java/com/example/demo/Calculator.java
+++ src/main/java/com/example/demo/Calculator.java
@@ -11,4 +11,11 @@ public class Calculator {
     public int subtract(int a, int b) {
         return a - b;
     }
+
+    public int div(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
 }
